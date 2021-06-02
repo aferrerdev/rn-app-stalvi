e80dfe9ddeceddf99fd6e029e105f882df25beb9
Alex Ferrer López <aferrerdev@gmail.com>
2021-06-02 17:05:04 +0200

    Basic react-navigation. Created basic pages for the application

--- StalviApp/src/StackNavigator.tsx
+++ StalviApp/src/StackNavigator.tsx
@@ -2,16 +2,18 @@ import {enableScreens} from 'react-native-screens';
 import {createNativeStackNavigator} from 'react-native-screens/native-stack';
 import React from 'react';
 import HomeTabsPage from './pages/HomeTabsPage/HomeTabsPage';
+import LoginPage from './pages/LoginPage/LoginPage';
+import RegisterPage from './pages/RegisterPage/RegisterPage';
 
 enableScreens();
 const Stack = createNativeStackNavigator();
 
 const StackNavigator = () => {
   return (
-    <Stack.Navigator initialRouteName="Login">
+    <Stack.Navigator initialRouteName="Home">
       <Stack.Screen name="Home" component={HomeTabsPage} />
-      <Stack.Screen name="Register" component={HomeTabsPage} />
-      <Stack.Screen name="Login" component={HomeTabsPage} />
+      <Stack.Screen name="Register" component={RegisterPage} />
+      <Stack.Screen name="Login" component={LoginPage} />
     </Stack.Navigator>
   );
 };
