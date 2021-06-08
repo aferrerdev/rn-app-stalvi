3b116b016eaab0f88cc3c64ec9580d71f48163f1
Alex Ferrer López <aferrerdev@gmail.com>
2021-06-08 13:40:52 +0200

    Add redux async actions. Refactor some pages.

---
+++ StalviApp/src/components/TransactionsList/TransactionsList.theme.ts
@@ -0,0 +1,13 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  listView: {
+    flex: 1,
+    alignSelf: 'stretch',
+  },
+  header: {
+    fontSize: 16,
+    flex: 1,
+    alignSelf: 'stretch',
+  },
+});
